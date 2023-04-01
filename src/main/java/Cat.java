f3eac9602f6c31ba655352aedac4641bc0971775
KonstantinEvt <k-s-y@yandex.ru>
2023-04-01 05:29:44 +0300

    Task is solved

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private String voice;
+    Cat(){
+    }
+
+    public void setVoice(String voice) {
+        this.voice=voice;
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+
+}
